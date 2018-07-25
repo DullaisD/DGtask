83def21f12cca549a393cacc67bbccdb286dbb9b
DullaisD <dullaisd@gmail.com>
2018-07-25 15:29:01 +0300

    done with be

--- api/class/Question.php
+++ api/class/Question.php
@@ -7,13 +7,18 @@
  * Time: 1:36 PM
  */
 
-class Question {
+/* Created simple api for be, in my opinion there were no need for db as questions can be somewhat elegantly in json file.
+ * By editing that file (questions.json) you can add and remove questions from test. Created on PHP 5.6.31, currently i
+ * don't have dev server set up and this is what i had locally */
 
+class Question {
     private $action;
+    private $questions = null;
 
     function __construct() {
-        if(isset($_GET['question'])) {
-            $this->action = $this->clean($_GET['question']);
+
+        if(isset($_POST['question'])) {
+            $this->action = $this->clean($_POST['question']);
 
             if($this->action === 'get') {
                 $this->getQuestions();
@@ -27,10 +32,10 @@ class Question {
         }
     }
 
+    //Returns questions
     private function getQuestions() {
-
-        $questions = file_get_contents('./../questions.json');
-        $questions = json_decode($questions,true);
+        //Loads questions, if they are not loaded yet as well as creating private var
+        $questions = $this->loadQuestions(true);
 
         //So we are not responding with correct answers...
         foreach ($questions as $question => &$answers) {
@@ -42,41 +47,86 @@ class Question {
         $this->respond($questions);
     }
 
+    //Validates questions and returns mistakes
     private function validateQuestions() {
 
-        if(isset($_GET['data'])) {
-            $data = json_decode($_GET['data']);
+        if(isset($_POST['data'])) {
+            $data = json_decode($_POST['data'], true);
 
-            if (json_last_error() === JSON_ERROR_NONE) {
+            if (json_last_error() === 0) {
                 $mistakes = $this->validateAnswers($data);
 
-
+                $this->respond($mistakes);
             } else {
                 $this->invalidData();
             }
         }
     }
 
-    private function validateAnswers(array $data) {
+    //Loads question if there is need for it, if given true - returns questions as well
+    private function loadQuestions($return = false) {
 
-        return 3;
+        if($this->questions === null) {
+            $this->questions = file_get_contents('./../questions.json');
+            $this->questions = json_decode($this->questions,true);
+        }
+
+        //Return only if asked to
+        if($return) {
+            return $this->questions;
+        }
     }
 
-    private function invalidData() {
-        $this->error('Invalid input var given !');
+    //Responds with JSON
+    private function respond(array $response) {
+
+        header('Content-Type: application/json');
+        echo json_encode($response, JSON_PRETTY_PRINT);
     }
 
+    //Throws error
     private function error($msg = "Fatal error has occurred !") {
         $this->respond(array("error" => $msg));
         die;
     }
 
-    private function respond(array $response) {
 
-        header('Content-Type: application/json');
-        echo json_encode($response);
+    //Validates answers one by one and returns array with mistakes
+    private function validateAnswers(array $data) {
+        $this->loadQuestions();
+        $mistakes = array();
+
+        foreach ($this->questions as $question => $answers) {
+            $mistakes[$question] = array();
+
+            foreach ($answers as $key => $value) {
+
+                if(isset($data[$question][$key])) {
+
+                    if($value !== $data[$question][$key]) {
+
+                        array_push($mistakes[$question], array(
+                            'answer' => $key,
+                            'correct' => $value
+                        ));
+                    }
+                }
+                //If this questions dont exist, we are throwing error
+                else {
+                    $this->invalidData();
+                }
+            }
+        }
+
+        return $mistakes;
     }
 
+    //Common function for invalid data
+    private function invalidData() {
+        $this->error('Invalid input given !');
+    }
+
+    //Cleans string
     private function clean($data) {
 
         return trim(htmlspecialchars($data));
